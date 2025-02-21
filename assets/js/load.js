94d284b4919ef56906809625892b6d926946f9b8
pado-lab <122674395+pado-lab@users.noreply.github.com>
2025-02-21 15:36:17 +0900

    Add files via upload

---
+++ assets/js/load.js
@@ -0,0 +1,56 @@
+const tl = gsap.timeline({defaults: {ease: "power2.out"}});
+
+tl.to(".zoom-finger", {
+  opacity: "1",
+  scale: 1.2,
+  y: "0",
+
+  delay: 0.25,
+  duration: 0.4,
+});
+
+tl.to(".zoom-text", {
+  opacity: "1",
+  delay: 0.25,
+  duration: 0.25,
+});
+
+tl.to(".swipe-finger-1", {
+  opacity: "1",
+  scale: 1.2,
+  delay: 0.5,
+  duration: 0.25,
+});
+tl.to(".swipe-finger-2", {
+  opacity: "1",
+  scale: 1.2,
+  delay: 0.05,
+  duration: 0.25,
+});
+// tl.to(".swipe span", {
+//   opacity: "1",
+//   delay: 0.1,
+//   duration: 0.125,
+// });
+
+tl.to(".swipe-text", {
+  opacity: "1",
+  delay: 0.25,
+  duration: 0.25,
+});
+
+tl.to(".project__loading", {
+  opacity: "0",
+  delay: 1.5,
+  duration: 0.375,
+  display: "none",
+}); /* ,  */
+
+/* HOME PAGE CONTENT ANIMATION */
+// tl.fromTo("main", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
+$(".load__info")
+  .find("button")
+  .click(function () {
+    $(".project__loading").css("display", "none");
+  });
